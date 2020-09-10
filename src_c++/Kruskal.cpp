b614d60dcec0e5febcdf314ac019879ff7dc714d
1925chicken <minori.sano@yandex.com>
2020-09-10 14:03:06 +0900

    minimum_spanning_tree

---
+++ src_c++/Kruskal.cpp
@@ -0,0 +1,67 @@
+#include<algorithm>
+template <typename T>
+class UnionFind
+{
+public:
+    vector<T> par;
+    UnionFind(T n) : par(n, -1) {}
+    void init(T n)
+    {
+        for (usize i = 0; i < n; i++)
+        {
+            par[i] = -1;
+        }
+    }
+
+    i64 root(T x)
+    {
+        if (par[x] < 0)
+            return x;
+        else
+            return par[x] = root(par[x]);
+    }
+
+    bool same(T x, T y)
+    {
+        return root(x) == root(y);
+    }
+
+    bool merge(T x, T y)
+    {
+        x = root(x);
+        y = root(y);
+        if (x == y)
+            return false;
+        if (par[x] > par[y])
+            swap(x, y);
+        par[x] += par[y];
+        par[y] = x;
+        return true;
+    }
+
+    i64 size(T x)
+    {
+        return -par[root(x)];
+    }
+};
+
+template<typename T>
+class edge{
+public:
+    T from,to,cost;
+};
+
+template<typename T>
+T kruskal(vector<edge<T>> &edges,i64 V){
+    sort(edges.begin(),edges.end(),[](const edge<T> &a,const edge<T> &b){
+        return a.cost < b.cost;
+    });
+    UnionFind uft(V);
+    T res = 0;
+    for(edge<T> &e : edges){
+        if(!(uft.same(e.a,e.b))){
+            uft.merge(e.a,e.b);
+            res += e.cost;
+        }
+    }
+}
