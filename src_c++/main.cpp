2aea6e77e515f746b7e5663464924c4100331c6c
1925chicken <minori.sano@yandex.com>
2020-10-12 17:52:27 +0900

    template_of_main_function

--- src_c++/main.cpp
+++ src_c++/main.cpp
@@ -1,13 +1,22 @@
-#pragma GCC target("avx")
+#pragma GCC target("avx2")
 #pragma GCC optimize("O3")
 #pragma GCC optimize("unroll-loops")
-#include<bits/stdc++.h>
+#include<iostream>
+#include<cstdint>
+#include<cstddef>
+#include<vector>
+//#include<atcoder/all>
+//using namespace atcoder;
 using namespace std;
-#define rep(i, n) for (long long i = 0; i < (long long)(n); i++)
-using ll = long long;
-using P = pair<ll,ll>;
+using i32 = int_fast32_t;
+using i64 = int_fast64_t;
+using usize = uint_fast64_t;
+#define rep(i, n) for (usize i = 0; i < (usize)(n); i++)
+#define all(a) (a).begin(),(a).end()
+#define rall(a) (a).rbegin(),(a).rend()
+using P = pair<i64,i64>;
 
 int main(){
-ios::sync_with_stdio(false);
-std::cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
 }
